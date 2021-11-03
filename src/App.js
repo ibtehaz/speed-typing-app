2335c1a38b1971a288dbe9f14fb396223a27281c
ibtehaz <ibtehaz.utsay@gmail.com>
2021-11-03 14:57:22 -0700

    implement timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 
 function App() {
+  const [text, setText ] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+  const calculateWordCount = (text) => {
+    const wordsArr = text.trim().split(" ");
+    console.log(wordsArr.length);
+    const filteredWords = wordsArr.filter(word => word !== "");
+    return filteredWords.length;
+  }
+  useEffect(() => {
+    if(timeRemaining > 0) {
+    setTimeout(() => {
+      setTimeRemaining(time => time - 1);
+    }, 1000);
+    }
+  }, [timeRemaining]);
+
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea />
-      <h4>Time reminaing: ???</h4>
-      <button>Start</button>
+      <textarea value={text} onChange={handleChange} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
